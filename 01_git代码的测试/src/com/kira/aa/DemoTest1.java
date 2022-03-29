d35d85ade643b81d539415dfdfe7ed226feb20f3
code_more <73972917+ZhouZhanJi@users.noreply.github.com>
2022-03-30 00:11:43 +0800

    Update DemoTest1.java

--- 01_git代码的测试/src/com/kira/aa/DemoTest1.java
+++ 01_git代码的测试/src/com/kira/aa/DemoTest1.java
@@ -4,9 +4,9 @@ public class DemoTest1 {
     public static void main(String[] args) {
         System.out.println("11111111");
         System.out.println("11111111");
+        System.out.println("22222");
         System.out.println("11111111");
-        System.out.println("11111111");
-        System.out.println("11111111");
+        System.out.println("33333");
 
 
         System.out.println("----------");
