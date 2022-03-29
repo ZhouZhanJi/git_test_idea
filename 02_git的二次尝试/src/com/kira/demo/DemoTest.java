28ea4317b870b8a1ef3647821b87b0f1127df89f
ZhouZhanJi <1390524841@qq.com>
2022-03-30 00:03:47 +0800

    张三第二次提交此代码

--- 02_git的二次尝试/src/com/kira/demo/DemoTest.java
+++ 02_git的二次尝试/src/com/kira/demo/DemoTest.java
@@ -3,5 +3,6 @@ package com.kira.demo;
 public class DemoTest {
     public static void main(String[] args) {
         System.out.println("it would be all right");
+        System.out.println("88");
     }
 }
