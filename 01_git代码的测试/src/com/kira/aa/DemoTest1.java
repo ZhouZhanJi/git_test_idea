611aee124aa93614139a1fae186ede6dc132bc80
ZhouZhanJi <1390524841@qq.com>
2022-03-29 23:59:09 +0800

    张三第一次提交此代码

---
+++ 01_git代码的测试/src/com/kira/aa/DemoTest1.java
@@ -0,0 +1,16 @@
+package com.kira.aa;
+
+public class DemoTest1 {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+        System.out.println("11111111");
+
+
+        System.out.println("----------");
+
+        System.out.println("8888");
+    }
+}
